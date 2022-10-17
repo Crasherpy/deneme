cccc6a34658bc63ba820a952b49e9ccd6a80f9c3
Crasherpy <99914112+Crasherpy@users.noreply.github.com>
2022-10-17 22:28:55 +0300

    Add files via upload

---
+++ Neferian2.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python
+import os
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("Neferian")
+print("""
+Welcome to the Neferian Pentest Tool
+1) Paramspider
+2)Dalfox
+3)SQL Scanner
+Telegram:Neferian
+""")
+Scanning = raw_input("Select the tool you will use")
+if(Scanning == "1"):
+    target = raw_input("Target:")
+    os.system("python3 paramspider.py -d" + target)
+elif(Scanning=="2"):
+    os.system("./dalfox file prodaft.txt -b hahwul.xss.ht")
+elif(Scanning=="3"):
+    target2 = raw_input("Dork:")
+    os.system("python3 main.py -d" + target2)
