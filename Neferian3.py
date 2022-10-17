714deaf039cca3322f1380bc34dcbedb2a854eca
Crasherpy <99914112+Crasherpy@users.noreply.github.com>
2022-10-17 22:45:30 +0300

    Update Neferian3.py

--- Neferian3.py
+++ Neferian3.py
@@ -2,7 +2,7 @@
 import os
 os.system("apt-get install figlet")
 os.system("clear")
-os.system("Neferian")
+os.system("Figlet Neferian")
 print("""
 Welcome to the Neferian Pentest Tool
 1) Paramspider
@@ -13,9 +13,11 @@ Telegram:Neferian
 scanning = input("Select the tool you will use:")
 if(scanning == "1"):
     target = input("Target:")
+    os.system("cd ParamSpider-master")
     os.system("python3 paramspider.py -d" + target)
 elif(scanning=="2"):
     os.system("./dalfox file prodaft.txt -b hahwul.xss.ht")
 elif(scanning=="3"):
     target2 = input("Dork:")
+    os.system("cd SQL-scanner-master")
     os.system("python3 main.py -d" + target2)
